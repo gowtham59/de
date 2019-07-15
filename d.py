5ba31e7819d949e84ceb8130f7007ec3b1f66cfc
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-07-15 16:35:23 +0530

    Create d.py

---
+++ d.py
@@ -0,0 +1,7 @@
+list12=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+list22=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+v3=str(input())
+for s4 in range(20):
+  if(v3==list12[s4]):
+    print(list22[s4])
+    break
